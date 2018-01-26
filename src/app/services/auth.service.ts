65aad2a733d0069183875186d69e15f39b757587
azam munawar <azam.butt@fiveriverstech.com>
2018-01-26 18:41:38 +0500

    lkjl

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,13 +9,13 @@ export class AuthService {
   constructor(private http: HttpClient) {
   }
 
-  // private serializeObj(obj) {
-  //   const result = [];
-  //   for(const property in obj) {
-  //     result.push(encodeURIComponent(property) + '=' + encodeURIComponent(obj[property]));
-  //   }
-  //   return result.join('&');
-  // }
+  private serializeObj(obj) {
+    const result = [];
+    for (const property in obj) {
+      result.push(encodeURIComponent(property) + '=' + encodeURIComponent(obj[property]));
+    }
+    return result.join('&');
+  }
 
   islogin(): any {
     const userLogin = localStorage.getItem('api_key');
@@ -28,16 +28,20 @@ export class AuthService {
   }
 
   login(fromData): any {
-    this.loginstatus.next(true);
+    // this.loginstatus.next(true);
     // let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded'});
     // let options = new RequestOptions({ headers: headers});
-    // const header = new HttpHeaders();
+    let headers = new HttpHeaders(): any ;
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8').set('h2', 'v2');
     // const options = header.append('Content-Type', 'application/x-www-form-urlencoded');
-    // const body = this.serializeObj(fromData);
-    // console.log('body', body);
-    // this.http.post('http://localhost:8000/login', body, options).subscribe(data => {
+    const body = this.serializeObj(fromData);
+    console.log('body', body);
+    this.http.post('http://localhost:8000/login', fromData, headers).subscribe(data => {
+      console.log('data', data);
+
+    });
+    // this.http.get('http://localhost:8000').subscribe(data => {
     //   console.log('data', data);
-    //
     // });
   }
 }
